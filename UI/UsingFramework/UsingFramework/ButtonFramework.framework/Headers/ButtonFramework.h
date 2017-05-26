e70ada2ad7e929f1169ee20584ff1d0f695deb8c
zhangfanghui <fhzhang@lonshinetech.com>
2017-05-26 13:26:16 +0800

    custom button

---
+++ UI/UsingFramework/UsingFramework/ButtonFramework.framework/Headers/ButtonFramework.h
@@ -0,0 +1,19 @@
+//
+//  ButtonFramework.h
+//  ButtonFramework
+//
+//  Created by zhangfanghui on 2017/5/26.
+//  Copyright © 2017年 zhangfanghui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ButtonFramework.
+FOUNDATION_EXPORT double ButtonFrameworkVersionNumber;
+
+//! Project version string for ButtonFramework.
+FOUNDATION_EXPORT const unsigned char ButtonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ButtonFramework/PublicHeader.h>
+
+
---
+++ UI/UsingFramework/UsingFramework/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  UsingFramework
+//
+//  Created by zhangfanghui on 2017/5/26.
+//  Copyright © 2017年 zhangfanghui. All rights reserved.
+//
+
+import UIKit
+import ButtonFramework
+
+
+class ViewController: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
