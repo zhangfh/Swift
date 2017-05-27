e33cfc157c4442959252e5a3b7dd1c775fd32755
zhangfanghui <fhzhang@lonshinetech.com>
2017-05-27 09:49:17 +0800

    Using Xib in Storyboard

--- UI/SingleXib/SingleXib/CardView.swift
+++ UI/SingleXib/SingleXib/CardView.swift
@@ -8,14 +8,62 @@
 
 import UIKit
 
+@IBDesignable
 class CardView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    @IBInspectable var cornerRadius: CGFloat = 8
+    @IBInspectable var shadowOffsetWidth : Int = 0
+    @IBInspectable var shadowOffsetHeight: Int = 3
+    
+    @IBInspectable var shadowColor: UIColor = UIColor.red
+    @IBInspectable var shadowOpacity : Float = 0.5
+    
+    var contentView : UIView?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+    
+    func xibSetup() {
+        contentView = loadViewFromNib()
+        
+        // use bounds not frame or it'll be offset
+        contentView!.frame = bounds
+        
+        // Make the view stretch with containing view
+        contentView!.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleHeight]
+        
+        // Adding custom subview on top of our view (over any custom drawing > see note below)
+        addSubview(contentView!)
+    }
+    
+    func loadViewFromNib() -> UIView! {
+        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        
+        return view
+    }
+    
+    override func layoutSubviews() {
+        
+        layer.cornerRadius = cornerRadius
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        
+        layer.masksToBounds = false
+        layer.shadowColor = shadowColor.cgColor
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight)
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowPath = shadowPath.cgPath
+        
+        
+    }
 
 }
