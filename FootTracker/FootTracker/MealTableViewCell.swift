5d83391bd9c09c17591c95d432ac317258055da4
zhangfanghui <fhzhang@lonshinetech.com>
2017-06-27 15:33:13 +0800

    FootTracker ,包括导航控制器的切换，tableviewcontroller的切换，自定义控件

---
+++ FootTracker/FootTracker/MealTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  MealTableViewCell.swift
+//  FootTracker
+//
+//  Created by zhangfanghui on 2017/6/26.
+//  Copyright © 2017年 zhangfanghui. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
